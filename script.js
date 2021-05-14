d3fe75fd94a7086be6116aa1405b937527958a3f
Bernadette McKnight <sissyhanks@yahoo.com>
2021-05-14 19:55:41 -0400

    final password generation

--- script.js
+++ script.js
@@ -1,38 +1,41 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// length of password as declared by user in prompt window
+var lengthEntry;
+// // while condition runs until a number between 8 - 128 is entered into prompt window
+while (!(lengthEntry >= 8 && lengthEntry <= 128)){
+    lengthEntry = Number(window.prompt("How many characters long would you like your password to be? Please enter a number between 8 and 128.", ""));
+  }
+
 // number of true (affirmative) responses to the criteria confirmation windows
 var count = 0;
 
+// password character type criteria variables to be declared by action confirmation windows
+var lowerChoice;
+var upperChoice;
+var numberChoice;
+var specialChoice;
+
 // arrays built from single characters returned from affirmative criteria confirmation and all of the characters included in affirmative criteria choices
 var lockedCriteriaCharacters = [];
+var lockedRemainderCharacters = [];
 var allCriteriaCharacters = [];
+var password = [];
 
-// number of characters user requests for password from prompt window that returns a number
-let lengthEntry = window.prompt("How many characters long would you like your password to be? Please enter a number between 8 and 128.");
+var remainderLength;
 
-var length = lengthEntry;
+var needNumber = window.alert("Please chose at least one character type for your " + lengthEntry + " character password by clicking OK when you see the type(s) of characters you would like included in your password from the pop up boxes to follow.");
 
-// while condition runs until a number between 8 - 128 is returned
-if (length >= 8 && length <= 128){
-  console.log("length works");
-}
-else {
-  while (!(length >= 8 && length <= 128)){
-    length = Number(window.prompt("You must enter a number between 8 and 128 to continue.", ""));
-  }
-}
+lowerChoice = window.confirm("LOWERCASE LETTERS \nWould you like your password to include lowercase letters? \nClick OK to include lower case letters in your password. \nClick Cancel to create a password without lower case letters.");
 
-// password character type criteria confirmation windows
-let lowerChoice = window.confirm("LOWERCASE LETTERS \nWould you like your password to include lowercase letters? \nClick OK to include lower case letters in your password. \nClick Cancel to create a password without lower case letters.");
+upperChoice = window.confirm("UPPERCASE LETTERS \nWould you like your password to include uppercase letters? \nClick OK to include uppercase letters in your password. \nClick Cancel to create a password without uppercase letters.");
 
-let upperChoice = window.confirm("UPPERCASE LETTERS \nWould you like your password to include uppercase letters? \nClick OK to include uppercase letters in your password. \nClick Cancel to create a password without uppercase letters.");
+numberChoice = window.confirm("NUMBERS \nWould you like your password to include numbers? \nClick OK to include numbers in your password. \nClick Cancel to create a password without numbers.");
 
-let numberChoice = window.confirm("NUMBERS \nWould you like your password to include numbers? \nClick OK to include numbers in your password. \nClick Cancel to create a password without numbers.");
+specialChoice = window.confirm("SPECIAL CHARACTERS \nWould you like your password to include special characters? \nClick OK to include special characters in your password. \nClick Cancel to create a password without special characters.");
 
-let specialChoice = window.confirm("SPECIAL CHARACTERS \nWould you like your password to include special characters? \nClick OK to include special characters in your password. \nClick Cancel to create a password without special characters.");
-
-// objects corresponding to individual character type criteria choices 
+// objects created based on password criteria options
 var lower ={
   criteria: lowerChoice,
 	characters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
@@ -94,10 +97,10 @@ var upper ={
 }
 
 var number ={
+
 	criteria: numberChoice,
 	characters: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
 	criteriaCharacter: [],
-
 
   increaseCount: function(){
     if (this.criteria === true){
@@ -125,8 +128,9 @@ var number ={
 }
 
 var special ={
+  
 	criteria: specialChoice,
-	characters: ["!", "#", "$", "%", "&", "'", "", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~",  "\"" ],
+	characters: ["!", "#", "$", "%", "&", "'", "", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "\`", "{", "|", "}", "~",  "\"" ],
 	criteriaCharacter : [],
 
   increaseCount: function(){
@@ -154,7 +158,6 @@ var special ={
   }
 }
 
-// calls all object functions
 lower.increaseCount();
 lower.pickCriteriaCharacter();
 lower.lockInCriteriaCharacter();
@@ -175,38 +178,40 @@ special.pickCriteriaCharacter();
 special.lockInCriteriaCharacter();
 special.assembleAllCriteriaCharacters();
 
+remainderLength = lengthEntry - count;
+
+function randomCharacter() {
+      lockedRemainderCharacters = lockedRemainderCharacters.concat(allCriteriaCharacters[Math.floor(Math.random() * allCriteriaCharacters.length)]);
+    }
+
+for (let i = 0; i < remainderLength; i++) {
+  randomCharacter();
+}
+
+password = lockedRemainderCharacters.concat(lockedCriteriaCharacters);
+password.sort(() => Math.random() - 0.5);
+
 // test outputs
-console.log(length);
+console.log(lengthEntry);
 console.log(count);
 console.log(lockedCriteriaCharacters);
 console.log(allCriteriaCharacters);
-
-// //       window.alert("Please chose at least one character type for your " + length + " character password by clicking OK when you see the type(s) of characters you would like included in your password from the pop up boxes to follow.");
-
+console.log(remainderLength);
+console.log(lockedRemainderCharacters);
+console.log(password);
 
 // /*
 // -------------------------------------------------------------------------
 // ------------------------- TO DO LIST ------------------------
 // -------------------------------------------------------------------------
 // TODO validate criteria choice ::::: at least one affirmative choice required 
-// /*
-// TODO random generation
-//   ** find remainderLength by subtracting count from length 
-//   ** generate remainderLength number of random characters from paramterCharacters array
-  
-//   ** randomly combine initialCharacterReturn and remainderCharacterReturn arrays into passwordReturn
-
 //   ** write passwordReturn to #password text box
 // */
-
-
 // Write password to the #password input
+
 function writePassword() {
-  var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  passwordText.value = password.join('');
 }
 
 // Add event listener to generate button
