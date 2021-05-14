6b2d268770e090ffcd4a0325dcaec21975d3e65f
Bernadette McKnight <sissyhanks@yahoo.com>
2021-05-13 22:46:43 -0400

    confirmation window return functionality

--- script.js
+++ script.js
@@ -1,39 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// number of characters user requests for password from prompt window that returns a number
-var length =  Number(window.prompt("How many characters long would you like your password to be? Please enter a number between 8 and 128.", ""));
-
-if (length >= 8 && length <= 128)
-      {
-        console.log("this works");
-      }
-else {
-  while (!(length >= 8 && length <= 128))
-    {
-      length = Number(window.prompt("You must enter a number between 8 and 128 to continue.", ""));
-    }
-}
-
-console.log(length);
-
-
-/* 
-*?* should i make this an object named parameters *?*
-password parameters as selected by user from varInput conformation windows that return booleans 
-*/
-var lower = Boolean(window.confirm("LOWERCASE LETTERS Would you like your password to include lowercase letters? Click OK to include lower case letters in your password. Click Cancel to create a password without lower case letters."));
-var upper = Boolean(window.confirm("UPPERCASE LETTERS Would you like your password to include uppercase letters? Click OK to include uppercase letters in your password. Click Cancel to create a password without uppercase letters."));
-var number = Boolean(window.confirm("NUMBERS Would you like your password to include numbers? Click OK to include numbers in your password. Click Cancel to create a password without numbers."));
-var symbol = Boolean(window.confirm("SPECIAL CHARACTERS Would you like your password to include special characters? Click OK to include special characters in your password. Click Cancel to create a password without special characters."));
-
-// parameters = function ()
-
-// number of true (affirmative) responses to the varInput confirmation windows
-var count = 0;
-
 /*
-*?* should i make this an object named parameterCharacterChoices *?*
 arrays of characters to randomly grab from 
 I used the method Array.from(); to generate an array from strings of characters that i then copied from the console log
 var string = "string of characters to turn into an array";
@@ -44,23 +12,83 @@ var upperCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L
 var numberCharacters = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var specialCharacters = ["!", "#", "$", "%", "&", "'", "", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~",  "\"" ];
 
-// an array made up of items from all parameterCharacterChoices that correspond to true paramter returns
-var paramterCharacters = []
 
-/*
-arrays of randomly generated characters to be randomly combined
-*/
-/*
-initialCharacterReturn are the single characters returned from each individual array list when it's corresponding character paramter is selected from varInput confirmation window prompts
-*/
-/*
-remainderCharacterReturn is an array of random characters generated from paramterCharacters
-*/
+// array of each single character returned after each true paramter choice
 var initialCharacterReturn = [];
+
+// array of all values from character arrays that correspond to true paramter choices
 var remainderCharacterReturn = [];
 
-// random combination of initialCharacterReturn and remainderCharacterReturn array items
-var passwordReturn = [];
+// random array of remainderCharacterReturn items that is length.length - count characters long
+var randomRemainderReturn = [];
+
+// random combination of initialCharacterReturn and randomRemainderReturn array items
+var passwordReturn =[]
+
+// number of characters user requests for password from prompt window that returns a number
+var length =  Number(window.prompt("How many characters long would you like your password to be? Please enter a number between 8 and 128.", ""));
+
+if (length >= 8 && length <= 128)
+      {
+        console.log("length works");
+      }
+else {
+  while (!(length >= 8 && length <= 128))
+    {
+      length = Number(window.prompt("You must enter a number between 8 and 128 to continue.", ""));
+    }
+}
+
+console.log(length);
+
+var initialLowerCharacter = lowerCharacters[Math.floor(Math.random() * lowerCharacters.length)];
+
+// number of true (affirmative) responses to the varInput confirmation windows
+var count = 0;
+
+/* 
+password parameters as selected by user from varInput conformation windows that return booleans 
+*/
+var lower = Boolean(window.confirm("LOWERCASE LETTERS \nWould you like your password to include lowercase letters? \nClick OK to include lower case letters in your password. \nClick Cancel to create a password without lower case letters."));
+
+if (lower === true){
+  var initialLowerCharacter = lowerCharacters[Math.floor(Math.random() * lowerCharacters.length)];
+  (count++); console.log(initialLowerCharacter); initialCharacterReturn.push(initialLowerCharacter);
+  remainderCharacterReturn = lowerCharacters.concat(remainderCharacterReturn);
+}
+
+
+var upper = Boolean(window.confirm("UPPERCASE LETTERS \nWould you like your password to include uppercase letters? \nClick OK to include uppercase letters in your password. \nClick Cancel to create a password without uppercase letters."));
+
+if (upper === true){
+  var initialUpperCharacter = upperCharacters[Math.floor(Math.random() * upperCharacters.length)];
+  (count++); console.log(initialUpperCharacter); initialCharacterReturn.push(initialUpperCharacter);
+  remainderCharacterReturn = upperCharacters.concat(remainderCharacterReturn); 
+}
+
+
+var number = Boolean(window.confirm("NUMBERS \nWould you like your password to include numbers? \nClick OK to include numbers in your password. \nClick Cancel to create a password without numbers."));
+
+if (number === true){
+  var initialNumberCharacter = numberCharacters[Math.floor(Math.random() * numberCharacters.length)];
+  (count++); console.log(initialNumberCharacter); initialCharacterReturn.push(initialNumberCharacter); remainderCharacterReturn = numberCharacters.concat(remainderCharacterReturn);
+}
+
+
+var symbol = Boolean(window.confirm("SPECIAL CHARACTERS \nWould you like your password to include special characters? \nClick OK to include special characters in your password. \nClick Cancel to create a password without special characters."));
+
+if (symbol === true){
+  var initialSpecialCharacter = specialCharacters[Math.floor(Math.random()) * specialCharacters.length]; (count++); console.log(initialSpecialCharacter); initialCharacterReturn.push(initialSpecialCharacter); remainderCharacterReturn = specialCharacters.concat(remainderCharacterReturn);
+}
+
+
+console.log(remainderCharacterReturn);
+console.log(initialCharacterReturn);
+console.log(count);
+
+
+
+
 
 
 /*
